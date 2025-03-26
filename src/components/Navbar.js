320ae8cdd5b2f6a7f777a0338440302523f964de
Rahul Verma <rahulverma.1.2005@gmail.com>
2025-03-26 08:44:25 +0530

    adding seo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import "../styles/Navbar.scss";
 const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className="navbar">
-      <button onClick={toggleSidebar} className="menu-btn">☰ Menu</button>
+      <button onClick={toggleSidebar} className="menu-button">☰ Menu</button>
       <p className="api-name app-name">PushNotify API</p>      
     </nav>
   );
